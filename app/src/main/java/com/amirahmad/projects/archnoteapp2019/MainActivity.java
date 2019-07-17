b2576489a5803c84969d1e02818a358a4e7e0f06
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-17 11:50:58 +0430

    git initial commit

---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amirahmad.projects.archnoteapp2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
