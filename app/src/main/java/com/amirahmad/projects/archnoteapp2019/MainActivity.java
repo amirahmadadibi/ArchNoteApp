b1dbd1444f03c2631636b64effdd6912ff472b25
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-19 15:54:27 +0430

    create and initialzie viewmodel

--- app/src/main/java/com/amirahmad/projects/archnoteapp2019/MainActivity.java
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/MainActivity.java
@@ -2,40 +2,47 @@ package com.amirahmad.projects.archnoteapp2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.amirahmad.projects.archnoteapp2019.model.NoteEntity;
 import com.amirahmad.projects.archnoteapp2019.ui.NotesAdapter;
 import com.amirahmad.projects.archnoteapp2019.utilities.SampleData;
+import com.amirahmad.projects.archnoteapp2019.viewmodel.MainViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
-//import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private MainViewModel mainViewModel;
     Toolbar toolbarNotes;
     RecyclerView recyclerViewNotes;
+    NotesAdapter notesAdapter;
     List<NoteEntity> noteEntityList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel  = ViewModelProviders.of(this).get(MainViewModel.class);
+        initViews();
+
+    }
+
+    private void initViews() {
         toolbarNotes = findViewById(R.id.toolbarNotes);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
         setSupportActionBar(toolbarNotes);
         noteEntityList = SampleData.getNotes();
         recyclerViewNotes.setHasFixedSize(true);
-        NotesAdapter notesAdapter = new NotesAdapter(noteEntityList);
+        notesAdapter = new NotesAdapter(noteEntityList);
         recyclerViewNotes.setAdapter(notesAdapter);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
-
     }
 
     @Override
@@ -48,7 +55,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.actionAddItem:
-                Toast.makeText(this, "yes", Toast.LENGTH_SHORT).show();
+                noteEntityList.addAll(SampleData.getNotes());
+                notesAdapter.notifyDataSetChanged();
+                break;
+            case R.id.actionRemoveAllItems:
+                noteEntityList.removeAll(noteEntityList);
+                notesAdapter.notifyDataSetChanged();
                 break;
             default:
                 Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/viewmodel/MainViewModel.java
@@ -0,0 +1,16 @@
+package com.amirahmad.projects.archnoteapp2019.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+/**
+ * Created by Amirahmad Adibi.
+ * ArchNoteApp2019 | Copyrights 2019-07-19.
+ */
+public class MainViewModel extends AndroidViewModel {
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
