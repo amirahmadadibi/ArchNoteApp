475cd44434edaf33dd4528dfc26be2ac4612d74e
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-19 14:03:54 +0430

    add typeConvert

---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/database/DateConverter.java
@@ -0,0 +1,8 @@
+package com.amirahmad.projects.archnoteapp2019.database;
+
+/**
+ * Created by Amirahmad Adibi.
+ * ArchNoteApp2019 | Copyrights 2019-07-19.
+ */
+public class DateConverter {
+}
---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/ui/NotesAdapter.java
@@ -0,0 +1,8 @@
+package com.amirahmad.projects.archnoteapp2019.ui;
+
+/**
+ * Created by Amirahmad Adibi.
+ * ArchNoteApp2019 | Copyrights 2019-07-17.
+ */
+public class NotesAdapter {
+}
