ef3a5240618d5919f0edcee04daf82c2b8cc795f
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-21 12:39:31 +0430

    using mutableLiveData calligraphy

---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.amirahmad.projects.archnoteapp2019;
+
+/**
+ * Created by Amirahmad Adibi.
+ * ArchNoteApp2019 | Copyrights 2019-07-21.
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/amirahmad/projects/archnoteapp2019/utilities/Constants.java
@@ -0,0 +1,8 @@
+package com.amirahmad.projects.archnoteapp2019.utilities;
+
+/**
+ * Created by Amirahmad Adibi.
+ * ArchNoteApp2019 | Copyrights 2019-07-20.
+ */
+public class Constants {
+}
